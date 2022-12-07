d8aa034c7f319ab567163d468b16993c7d313d4a
Psalmdave <miracleachumba@gmail.com>
2022-12-07 05:19:20 -0800

    1-search_replace.py main

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,11 +5,11 @@ def search_replace(my_list, search, replace):
     return(new_list)
 
 '''
-    new_list = []
-    for x in my_list:
-        if val == search:
-            new_list.append(replace)
-        else:
-            new_list.append(x)
-    return new_list
+new_list = []
+for x in my_list:
+if val == search:
+new_list.append(replace)
+else:
+new_list.append(x)
+return new_list
 '''
